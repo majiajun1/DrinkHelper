776b0bc507b72b97eff5e23af49f2a91761861a3
majiajun <masonjj@foxmail.com>
2025-12-12 17:42:58 +0800

    init

---
+++ app/src/main/java/com/example/drinkhelper/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.drinkhelper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
