28c5c78e5e45084621d1a2e36036d72b78b2607a
majiajun <masonjj@foxmail.com>
2025-12-13 17:36:34 +0800

    202512131736

--- app/src/main/java/com/example/drinkhelper/MainActivity.java
+++ app/src/main/java/com/example/drinkhelper/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.drinkhelper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.renderscript.ScriptGroup;
 
+import com.example.drinkhelper.databinding.MainpageBinding;
 
 
 public class MainActivity extends AppCompatActivity{
 
+    private MainpageBinding binding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.mainpage);
     }
+
+
 }
