6eeeaa148b04c14cf10010830728c6ac1b567541
majiajun <masonjj@foxmail.com>
2025-12-13 17:18:29 +0800

    202512131718

--- app/src/main/java/com/example/drinkhelper/MainActivity.java
+++ app/src/main/java/com/example/drinkhelper/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity{
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.mainpage);
     }
 }
